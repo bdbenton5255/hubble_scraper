088be9c68abecf23638a728b368910fd08725660
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-07 00:53:00 -0500

    pulling script tag out of page

--- hubble_scraper.py
+++ hubble_scraper.py
@@ -1,11 +1,11 @@
+import re
 import requests
 from bs4 import BeautifulSoup
-import os
 
-for i in range(37):
+url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+image_script = soup.find('script')
+image_links = re.findall(r"/images/*", str(image_script))
 
-    url = "https://esahubble.org/images/archive/category/galaxies/page/" + str(i + 1)
-    r  = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    print(soup.title.text + " page: " + str(i + 1))
+print(image_links)
