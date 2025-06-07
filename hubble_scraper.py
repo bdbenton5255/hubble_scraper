c1addc2683842e050a14d05afeae810ce18c6100
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-07 02:13:21 -0500

    pulling high rest image links from each image pages link

--- hubble_scraper.py
+++ hubble_scraper.py
@@ -3,6 +3,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+
 url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
@@ -10,8 +11,22 @@ image_script = soup.find('script')
 image_links = re.findall(r"url\s*:\s*'([^']+)'", str(image_script))
 
 for links in image_links:
+
     sub_url = "https://esahubble.org" + links
-    reqs = requests.get(url)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
+    sub_reqs = requests.get(sub_url)
+    sub_soup = BeautifulSoup(sub_reqs.text, 'html.parser')
     time.sleep(1)
+    
     print(sub_url)
+
+    for link in sub_soup.find_all('a', href=True, string='Large JPEG'):
+        print(link.get('href'))
+
+
+#sub_url = 'https://esahubble.org/images/heic2018b/'
+#reqs = requests.get(url)
+#soup = BeautifulSoup(reqs.text, 'html.parser')
+#links = soup.find_all('a href="https://cdn.esahubble.org/archives/images/large')
+
+#for link in soup.find_all('a', href=True, string='Large JPEG'):
+#    print(link.get('href'))
