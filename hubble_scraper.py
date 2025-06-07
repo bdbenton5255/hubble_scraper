fa8207d88f8af69748eec8fbcdb123d52b3b21c1
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-07 03:56:04 -0500

    status messages

--- hubble_scraper.py
+++ hubble_scraper.py
@@ -1,7 +1,14 @@
 import re
+import os
 import time
+import urllib
 import requests
 from bs4 import BeautifulSoup
+
+try:
+    os.mkdir("galaxy_images")
+except FileExistsError:
+    print("galaxy_images directory already exists.")
 
 
 url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
@@ -9,6 +16,7 @@ reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 image_script = soup.find('script')
 image_links = re.findall(r"url\s*:\s*'([^']+)'", str(image_script))
+image_number = 0
 
 for links in image_links:
 
@@ -17,16 +25,16 @@ for links in image_links:
     sub_soup = BeautifulSoup(sub_reqs.text, 'html.parser')
     time.sleep(1)
     
-    print(sub_url)
+    print("Image page: " + str(sub_url))
 
     for link in sub_soup.find_all('a', href=True, string='Large JPEG'):
-        print(link.get('href'))
+        
+        high_res_image_link = link.get('href')
+        print("Large JPEG link: " + str(high_res_image_link))
 
+        time.sleep(1)
 
-#sub_url = 'https://esahubble.org/images/heic2018b/'
-#reqs = requests.get(url)
-#soup = BeautifulSoup(reqs.text, 'html.parser')
-#links = soup.find_all('a href="https://cdn.esahubble.org/archives/images/large')
+        #urllib.request.urlretrieve(high_res_image_link, "galaxy_images/galaxy_" + str(image_number) +".jpg")
 
-#for link in soup.find_all('a', href=True, string='Large JPEG'):
-#    print(link.get('href'))
+        print("Image " + "galaxy_" + str(image_number) + ".jpg downloaded!")
+        image_number += 1
