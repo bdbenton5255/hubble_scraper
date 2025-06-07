63f87536e3af4bf0dd65c4798dcead8a1315cc9f
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-07 01:42:35 -0500

    pulling image page links

--- hubble_scraper.py
+++ hubble_scraper.py
@@ -1,4 +1,5 @@
 import re
+import time
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,6 +7,11 @@ url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 image_script = soup.find('script')
-image_links = re.findall(r"/images/*", str(image_script))
+image_links = re.findall(r"url\s*:\s*'([^']+)'", str(image_script))
 
-print(image_links)
+for links in image_links:
+    sub_url = "https://esahubble.org" + links
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    time.sleep(1)
+    print(sub_url)
