f34c276b407ae19d3ed9b4edf15e3d4935965781
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-08 17:31:46 -0500

    iterate through all pages

--- hubble_scraper.py
+++ hubble_scraper.py
@@ -11,34 +11,41 @@ try:
 except FileExistsError:
     print("galaxy_images directory already exists.")
 
+for pages in range(37):
 
-url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-image_script = soup.find('script')
-image_links = re.findall(r"url\s*:\s*'([^']+)'", str(image_script))
-image_number = 0
+    url = 'https://esahubble.org/images/archive/category/galaxies/page/' + str(pages+1)
 
-for links in image_links:
+    print("Downloading images from page " + str(pages+1))
 
-    sub_url = "https://esahubble.org" + links
-    sub_reqs = requests.get(sub_url)
-    sub_soup = BeautifulSoup(sub_reqs.text, 'html.parser')
-    time.sleep(1)
-    
-    print("Image page: " + str(sub_url))
+    url = 'https://esahubble.org/images/archive/category/galaxies/page/1/'
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    image_script = soup.find('script')
+    image_links = re.findall(r"url\s*:\s*'([^']+)'", str(image_script))
+    image_number = 0
 
-    for link in sub_soup.find_all('a', href=True, string='Large JPEG'):
-        
-        high_res_image_link = link.get('href')
-        print("Large JPEG link: " + str(high_res_image_link))
+    for links in image_links:
 
+        sub_url = "https://esahubble.org" + links
+        sub_reqs = requests.get(sub_url)
+        sub_soup = BeautifulSoup(sub_reqs.text, 'html.parser')
         time.sleep(1)
+        
+        print("Image page: " + str(sub_url))
 
-        opener = urllib.request.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(high_res_image_link, "galaxy_images/galaxy_" + str(image_number) +".jpg")
+        for link in sub_soup.find_all('a', href=True, string='Large JPEG'):
+            
+            high_res_image_link = link.get('href')
+            print("Large JPEG link: " + str(high_res_image_link))
 
-        print("Image " + "galaxy_" + str(image_number) + ".jpg downloaded!")
-        image_number += 1
+            time.sleep(1)
+
+            opener = urllib.request.build_opener()
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+            urllib.request.install_opener(opener)
+            urllib.request.urlretrieve(high_res_image_link, "galaxy_images/galaxy_" + str(image_number) +".jpg")
+
+            print("Image " + "galaxy_" + str(image_number) + ".jpg downloaded!")
+            image_number += 1
+    
+    pages += 1
